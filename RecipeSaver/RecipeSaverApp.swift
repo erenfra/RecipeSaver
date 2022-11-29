6e748a8c207d784670020a1bedf75194d791898b
Renato Oliveira Fraga <rofraga74@gmail.com>
2022-11-29 12:47:38 -0600

    Initial Commit

---
+++ RecipeSaver/RecipeSaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeSaverApp.swift
+//  RecipeSaver
+//
+//  Created by Renato Oliveira Fraga on 11/29/22.
+//
+
+import SwiftUI
+
+@main
+struct RecipeSaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
